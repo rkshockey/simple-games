1e7a13360d308f4d14c5012b320f7d53a1ecd2c1
rkshockey <rkshockey@gmail.com>
2021-07-31 22:05:08 -0400

    Started on barfight

---
+++ assets/constants.js
@@ -0,0 +1,44 @@
+const moveSet = [
+    {
+      name: "uppercut", 
+      damage: 30, 
+      toHit: 0.8, 
+      defense: 0, 
+      heal: 0, 
+      dodge: 0
+    },
+    {
+      name: "slap", 
+      damage: 20, 
+      toHit: 0.9, 
+      defense: 0, 
+      heal: 0, 
+      dodge: 0
+    },
+    {
+      name: "defend", 
+      damage: 0, 
+      toHit: 1, 
+      defense: 10, 
+      heal:0, 
+      dodge: 0.1
+    },
+    {
+      name: "heal", 
+      damage: 0, 
+      toHit: 1, 
+      defense: 5, 
+      heal: 20, 
+      dodge: 0
+    },
+    {
+      name: "dodge", 
+      damage: 0, 
+      toHit: 1, 
+      defense: 5, 
+      heal: 0, 
+      dodge: 0.5
+    },
+  ];
+
+export { moveSet }
---
+++ src/components/barfight/Barfight.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+function Barfight (props) {
+    const [pHP, setPHP] = useState(200)
+    const [eHP, setEHP] = useState(200)
+
+    return null
+}
+
+export default Barfight
